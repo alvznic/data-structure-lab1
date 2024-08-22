7226b323e20a0aff102b6cb50e382e1213eedaad
Nicole Alvarez <alvareznicole962@gmail.com>
2024-08-22 12:07:45 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+number1 = input("Enter first number: ")
+number2 = input("Enter second number: ")
+
+if number1 < number2:
+    print(f"{number1} is less than {number2}")
+elif number1 > number2:
+    print(f"{number1} is greater than {number2}")
+elif number1 == number1:
+    print(f"{number1} is equals to {number2}")
+
+
+# cmate1 = input("Enter classmate name 1: ")
+# cmate2 = input("Enter classmate name 2: ")
+# cmate3 = input("Enter classmate name 3: ")
+# print(f"Your classmates are: {cmate1}, {cmate2}, & {cmate3}")
+
